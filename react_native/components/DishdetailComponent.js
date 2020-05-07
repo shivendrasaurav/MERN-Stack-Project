c56eead97d1702fdec278055c2758f1561054138
Shivendra Saurav <shivendrasaurav@gmail.com>
2020-05-07 11:42:08 +0530

    Nav not working

---
+++ react_native/components/DishdetailComponent.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+import { Card } from 'react-native-elements';
+import { DISHES } from '../shared/dishes';
+
+class DishDetail extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            dishes: DISHES
+        };
+    }
+
+    static navigationOptions = {
+        title: 'Dish Details'
+    };
+
+    render() {
+        const dishId = this.props.navigation.getParam('dishId','');
+        return(
+            <RenderDish dish={this.state.dishes[+dishId]} />
+        );
+    }
+}
+
+export default Dishdetail;
