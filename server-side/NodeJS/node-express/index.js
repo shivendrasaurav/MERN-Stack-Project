c4923b8792e42fe5d74e2e5dacc8159c66965a97
Shivendra Saurav <shivendrasaurav@gmail.com>
2020-05-14 20:58:27 +0530

    Node Routes

--- server-side/NodeJS/node-express/index.js
+++ server-side/NodeJS/node-express/index.js
@@ -1,23 +1,25 @@
-var rect = require('./rectangle');
+const express = require('express'),
+     http = require('http');
 
-function solveRect(l,b) {
-    console.log("Solving for rectangle with l = "
-                + l + " and b = " + b);
-    rect(l,b, (err,rectangle) => {
-        if (err) {
-	        console.log("ERROR: ", err.message);
-	    }
-        else {
-            console.log("The area of the rectangle of dimensions l = "
-                + l + " and b = " + b + " is " + rectangle.area());
-            console.log("The perimeter of the rectangle of dimensions l = "
-                + l + " and b = " + b + " is " + rectangle.perimeter());
-        }
-    });
-    console.log("This statement after the call to rect()");
-};
+const hostname = 'localhost';
+const port = 3000;
 
-solveRect(2,4);
-solveRect(3,5);
-solveRect(0,5);
-solveRect(-3,5);
+const morgan = require('morgan');
+
+const bodyParser = require('body-parser');
+const app = express();
+
+const dishRouter = require('./routes/dishRouter');
+app.use('/dishes', dishRouter);
+
+const promoRouter = require('./routes/promoRouter');
+app.use('/promotions', promoRouter);
+
+const leaderRouter = require('./routes/leaderRouter');
+app.use('/leaders', leaderRouter);
+
+const server = http.createServer(app);
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
---
+++ server-side/NodeJS/node-express/routes/leaderRouter.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const leaderRouter = express.Router();
+
+leaderRouter.use(bodyParser.json());
+
+leaderRouter.route('/')
+.all((req,res,next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+})
+.get((req,res,next) => {
+    res.end('Will send all the leaders to you!');
+})
+.post((req, res, next) => {
+    res.end('Will add the leader: ' + req.body.name + ' with details: ' + req.body.description);
+})
+.put((req, res, next) => {
+    res.statusCode = 403;
+    res.end('PUT operation not supported on /leader');
+})
+.delete((req, res, next) => {
+    res.end('Deleting all leader');
+});
+
+leaderRouter.route('/:leaderId')
+    .all(function (req, res, next) {
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        next();
+    })
+
+    .get(function (req, res, next) {
+        res.end('Will send details of the leader: ' + req.params.leaderId + ' to you!');
+    })
+
+    .put(function (req, res, next) {
+        res.write('Updating the leader: ' + req.params.leaderId + '\n');
+        res.end('Will update the leader: ' + req.body.name +
+            ' with details: ' + req.body.description);
+    })
+
+    .delete(function (req, res, next) {
+        res.end('Deleting leader: ' + req.params.leaderId);
+    });
+
+module.exports = leaderRouter;
